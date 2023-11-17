436d3b67cb53611b3245dc0eafdeca40b9ac022d
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-11-17 15:48:41 -0300

    add: layout basico da screen Categoria

--- src/screen/Category/index.tsx
+++ src/screen/Category/index.tsx
@@ -1,9 +1,27 @@
-import { View,Text } from "react-native"
+import { View, Text, Image, SafeAreaView, FlatList, ScrollView } from "react-native"
+import { styles } from "./style"
+import { CategoryCard } from "../../components/CategoryCard"
 
 export const Category = () => {
-  return(
-    <View>
-        <Text>Bem vindo a Tela de Category</Text>
-    </View>
+  return (
+    <SafeAreaView>
+      <View style={styles.containerUpper}>
+        <Image style={{ marginTop: 30 }} source={require("../../assets/images/ShoinLogo.png")}></Image>
+      </View>
+      <View style={styles.containerLower}>
+        <Text style={styles.text}>Em Destaque</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.containerCards}>
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+        </View>
+        <View style={{marginBottom:200}}></View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
