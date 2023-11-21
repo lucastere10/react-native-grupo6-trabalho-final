ca1af922262c88df5305dd361d450818d3d1d47e
Lucas Caldas <lucasmedeiroscaldas@yahoo.com.br>
2023-11-21 16:53:55 -0300

    add: iniciando trabalho no modal

--- src/components/modalProduto/index.tsx
+++ src/components/modalProduto/index.tsx
@@ -1,6 +1,43 @@
 import { View, Text, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import { getProductDetails } from '../../service/api';
 import { styles } from './style';
 
-export const ModalProduto = () => {
-  
+interface ModalProductDetailsProps {
+	isModalVisible: boolean,
+	setIsModalVisible: React.Dispatch<React.SetStateAction<boolean>>,
+	id: number,
+}
+
+export const ModalProduto = ({ isModalVisible, setIsModalVisible, id }: ModalProductDetailsProps) => {
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [product, setProduct] = useState<getProductResponse>({
+		id: null,
+		title: "",
+		category: "",
+		description: "",
+		thumbnail: "",
+		brand: "",
+		price: null,
+		rating: null,
+	})
+
+	useEffect(() => {
+		writeProductDetails(id)
+	}, []);
+
+	function writeProductDetails(id: string) {
+		getProductResponse(id)
+			.then(response => {
+				setMagicItem(response.data);
+				magicItemWithPrice
+			})
+			.catch(error => {
+				console.log(error);
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	}
+
 }
